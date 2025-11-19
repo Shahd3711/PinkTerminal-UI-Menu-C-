be27651ed8ba26b7bd4b385f1426a309e6ea1a86
Shahd Mostafa <164599571+Shahd3711@users.noreply.github.com>
2025-11-19 14:29:31 +0200

    Update pix.h

--- pix.h
+++ pix.h
@@ -3,12 +3,21 @@
 
 #include <string>
 #include <utility>
+#include <vector>
+#include <termios.h>
+
+extern struct termios orig_termios;
 
 void clearScreen();
 void setColor(const std::string& colorName);
 void gotoXY(int x, int y);
 std::pair<int,int> getTerminalSize();
 void printCenter(const std::string& text, const std::string& color="white");
-void waitForEnter();
+int readKey();
+void waitForRawEnter();
+void flushInput();
+void enableRawMode();
+void disableRawMode();
+void drawMenu(const std::vector<std::string>& menu, int selected);
 
 #endif
